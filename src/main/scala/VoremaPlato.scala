110b80bdf745216eb9c0ceca0e65584565646d42
Koen van der Kruk <koenvdkruk@gmail.com>
2015-05-31 21:08:48 +0200

    New updates

--- src/main/scala/VoremaPlato.scala
+++ src/main/scala/VoremaPlato.scala
@@ -12,26 +12,37 @@ class VoremaPlato(editor: String, mediaPlayer: String, voiceRedDir: String){
       val fileContents = try fileOpen.getLines().toList finally fileOpen.close()
       if(!fileContents.isEmpty){
          val cursorLine = returnLine(fileContents, cursorPosition.row)
-
-         println("REGEX FOUND: "+ stripRecName(cursorLine))
+         val possibleRecNames = stripRecName(cursorLine.get())
+         val playFile = returnClosestRecName(cursorLine, cursorPosition.col, possibleRecNames)
+         println("Play this file: "+ playFile + " Because cursor was located at: " cursorPosition)
       }
 
    }
 
-   def returnLine(textLines: List[String], row: Int): String={
+   def returnLine(textLines: List[String], row: Int): Option[String]={
       textLines match{
-         case head :: tail if row == 1 => head
+         case head :: tail if row == 1 => Some(head)
          case head :: tail => returnLine(tail, row - 1)
-         case Nil => "[NOSUCHLINE]"
+         case Nil => None
+      }
+   }
+
+   def returnClosestRecName(textLine: String, col: Int, recnames: List[(String, String)]): Option[String] = {
+      val rangePosOfRec: List[(Int, Int)] = recnames.map(rec => (textLine.indexOfSlice(rec._1).toInt,textLine.indexOfSlice(rec._1)+rec._1.length.toInt))
+      val ColBetween: Int = rangePosOfRec.map(range => range._1 <= col && col <= range._2).indexOf(true)
+      ColBetween match{
+         case x: Int if(x >= 0 ) => Some(recnames(ColBetween)._2)
+         case _ => None   
       }
    }
 
 // date -d "$(echo "y2015_m04_d19_h15_m44_s49" | sed -e 's/_[h]/ /g' -e 's/[a-z]//g' -e 's/_/-/g' -e 's/-/:/3g')"
 
 
-   def stripRecName(procContents: String): Option[String] ={ //And yet another one here: VR{| y2015_m05_d17. |}
-      """(?s)VR\{\|(.*)\|\}""".r findFirstIn procContents
-      // echo "This is elucidated in more detail in VR{| y2015_m05_d17_h15_m16_s34. |} However, this contradicts what is said in VR{| y2015_m05_d14_o002. |}." | grep -Po '(?<=VR\{\|)(.*)(?=\|\})'
+   def stripRecName(procContents: String): List[(String, String)] ={ //And yet another one here: VR{| y2015_m05_d17. |}
+      val recnameMatches: List[String] = ("""VR\{\|(.*?)\|\}""".r findAllIn procContents toList)
+      val cleanedRec = recnameMatches.map(_.replace("VR{| ", "").replace(". |}",""))
+      recnameMatches zip cleanedRec
    }
 
    def playUnderCursor(): Unit ={
