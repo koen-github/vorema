59318846f9f9c0f980c356cc618aa69e735d57bb
Koen van der Kruk <koenvdkruk@gmail.com>
2015-07-31 23:11:43 +0200

    Added working vim example

--- src/main/scala/vorema.scala
+++ src/main/scala/vorema.scala
@@ -15,13 +15,44 @@ object Vorema {
                "\tARG4: row,col of cursor position\n")
 
       } else {
+         val editorName = args.toList(0)
+         val musicPlayer = args.toList(1)
+         val voiceDir = args.toList(2)
+         val filename = args.toList(3)
+         val rowCol = args.toList(4)
          println("You specified the following running options: ")
          println(
-            "ARG0: " + args.toList(0) + "\n" +
-               "ARG1: " + args.toList(1) + "\n" +
-               "ARG2: " + args.toList(2) + "\n" +
-               "ARG3: " + args.toList(3) + "\n" +
-               "ARG4: " + args.toList(4) + "\n")
+            "Editor: " + editorName + "\n" +
+               "musicPlayer: " + musicPlayer + "\n" +
+               "voiceDir: " + voiceDir + "\n" +
+               "filename: " + filename + "\n" +
+               "rowCol: " + rowCol + "\n")
+         editorName match{
+            case "emacs" => {
+
+               println("Todo, not yet finished. Have to find a way to convert the emacs position to a row and col position")
+
+               val curPos: CursorPos = CursorPos(3,3) //TODO: calculate the emacs character number to a row and col position
+
+               val voremo = new VoremaPlato("emacs", musicPlayer, voiceDir)
+               val fileName = voremo.voremaPlatoOpen(filename, curPos)
+               val exitValue = voremo.playFile(fileName._2.get, true)
+               println("Succes? " + exitValue)
+            }
+            case "vim" => {
+               val curPos: CursorPos = CursorPos(rowCol.split(",")(0).toInt,rowCol.split(",")(1).toInt)
+
+               val voremo = new VoremaPlato("vim", musicPlayer, voiceDir)
+               val fileName = voremo.voremaPlatoOpen(filename, curPos)
+               val exitValue = voremo.playFile(fileName._2.get, true)
+               println("Succes? " + exitValue)
+            }
+            case _ => println("You specified a non-supported editor. Please check you settings.")
+         }
+
+
+
+
       }
 
       //voremo.playUnderCursor(fileName.getOrElse("No file found"))
